f4d4a23148acebae792e86da1683ee78a1e2ff9f
Nikhil Vibhav <nikhil92ind@gmail.com>
2021-02-03 22:00:21 +0530

    Add a scaffold Spring Boot application

---
+++ src/main/java/io/assessment/banking/BankingApplication.java
@@ -0,0 +1,17 @@
+package io.assessment.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the Spring Boot application
+ *
+ * @author Nikhil Vibhav
+ */
+@SpringBootApplication
+public class BankingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BankingApplication.class, args);
+  }
+}
---
+++ src/test/java/io/assessment/banking/BankingApplicationTests.java
@@ -0,0 +1,16 @@
+package io.assessment.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Test cases for Banking application
+ *
+ * @author Nikhil Vibhav
+ */
+@SpringBootTest
+class BankingApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
